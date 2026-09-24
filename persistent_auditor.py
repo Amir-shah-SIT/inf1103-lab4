43bcf497d6709a203e69bd3b8c3b14a3a4fac804
Amir <2601448@sit.singaporetech.edu.sg>
2026-09-24 21:23:08 +0800

    list tracking history working

--- persistent_auditor.py
+++ persistent_auditor.py
@@ -1,54 +1,74 @@
 rejected = 0
-load_inventory = open("inventoryList", "w+")
-
+def load_inventory():
+    with open("inventoryList.txt", "w+") as file:
+        readInventory = file.read()
+        inventory = readInventory.split(",")
+        if inventory[0] == "":
+            inventory.pop()
+        print(inventory)
+        return inventory
+    
+def write_inventory():
+    with open('inventoryList.txt', 'w') as file:
+        for item in inventory:
+            file.write(','.join(item))
+            
 def get_valid_input():
-    valid_input = False
-    while valid_input == False: 
-        global rejected
-        userInput = input("\nInput additional stock value or quit:\n")
-        if userInput.isdigit() == True:
-            if int(userInput) < 0:
-                rejected += 1
-                print("\nUnacceptable input")
+    while True: 
+        global rejected 
+        productInput = input("\nEnter product name or quit:\n")
+        if productInput.lower() == "quit":
+            return productInput.lower()
+        elif productInput.isdigit() == False:
+            userInput = input("\nInput additional stock value or quit:\n")
+            if userInput.isdigit() == True:
+                if int(userInput) < 0:
+                    rejected += 1
+                    print("\nUnacceptable input")
+                else:
+                    return [productInput,userInput]
+            elif userInput.lower() == "quit":
+                return userInput.lower()
             else:
-                return userInput
-        elif userInput.lower() == "quit":
-            return userInput.lower()
+                rejected += 1
+                print("Unacceptable input")
         else:
             rejected += 1
-            print("Unacceptable input") 
+            print("Unacceptable input")
     
-def process_delivery(current_total, new_value):
-    current_total += int(new_value)
-    return current_total
+def process_delivery(inventoryTotal, new_value):
+    
+    if len(inventoryTotal) == 0:
+        new_value.insert(0,1001)
+    else:
+        new_value.insert(0,int(inventoryTotal[-1][0]) + 1) 
+    inventoryTotal.append(new_value)
+    print(inventoryTotal)
+    return inventoryTotal
 
-def calculate_tax(amount):
-    tax = amount * 2.5 * .10
-    return tax
+#def calculate_tax(amount):
+    #tax = amount * 2.5 * .10
+    #return tax
 
 def generate_report(total_units, failed_attempts):  
     print("\nTotal Deliveries Processed: " + str(total_units))
     print("\nNumber of Failed/Rejected Entries: " + str(failed_attempts))
     return 
 
+
+inventory = load_inventory()
 while True :
     #print("\nTotal Deliveries Processed: " + str(inventory))
     accepted_Input = get_valid_input()
     if accepted_Input == 'quit':
         generate_report(inventory,rejected)
-        tax = calculate_tax(inventory)
-        print("\n The tax amount is: $"+ str(tax))
+        #tax = calculate_tax(inventory)
+        #print("\n The tax amount is: $"+ str(tax))
         break
     else:
         inventory = process_delivery(inventory,accepted_Input)
-        if inventory >= 500:
-            generate_report(500,rejected)
-            print("\nNumber of Rejected Stock: "+ str(inventory-500))
-            tax = calculate_tax(500)
-            print("\n The tax amount is: $"+ str(tax))
-            break
-        tax = calculate_tax(inventory)
-        print("\n The tax amount is: $"+ str(tax))
+        #tax = calculate_tax(inventory)
+        #print("\n The tax amount is: $"+ str(tax))
         
             
     
